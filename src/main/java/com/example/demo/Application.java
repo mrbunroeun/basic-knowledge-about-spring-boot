510f4f1da7fb1ac6ee62890068090586d4296bee
mrbunroeun <bunroeunhas@gmail.com>
2026-08-19 11:10:10 +0700

    create some folder

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -3,11 +3,19 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @SpringBootApplication
-public class DemoApplication {
+@RestController
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
+	@GetMapping("/")
+	public String hello() {
+		return "Hello, Spring Boot is running!";
+	}
 }
